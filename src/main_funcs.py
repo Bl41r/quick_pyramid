ec0642a9c587b65cf241b6f0534f97cbb246cd26
bl41r <dbsmith.dbs83@gmail.com>
2017-01-26 22:58:45 -0800

    redoing generic route builder, readme update

--- src/main_funcs.py
+++ src/main_funcs.py
@@ -13,12 +13,4 @@ def create_directories(directory_names):
 
 def create_generic_route(route_name, renderer=None, permission=None):
     """Return text for a route."""
-    if permission:
-        permission = ", permission='" + permission + "'"
-    template = renderer
-    if renderer:
-        renderer = ", renderer='" + template + "'"
-
-    return '@view_config(route_name="' + route_name + "'" + renderer + \
-        permission + '")\n' + 'def ' + route_name + '(request):\n    ' +\
-        'return {}\n\n'
+    pass
