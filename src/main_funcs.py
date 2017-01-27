efac0f7148b42f6135c5527f85df7e26bfe0ed71
bl41r <dbsmith.dbs83@gmail.com>
2017-01-26 20:16:22 -0800

    route creation string

--- src/main_funcs.py
+++ src/main_funcs.py
@@ -9,3 +9,9 @@ def create_directories(directory_names):
     for directory in directory_names:
         if not os.path.exists(directory):
             os.makedirs(directory)
+
+
+def create_route(route_name):
+    """Return text for a route."""
+    return '@view_config(route_name="' + route_name + '")\n' + 'def ' +\
+        route_name + '(request):\n    ' + 'return {}\n\n'
