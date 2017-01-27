1b4f8281d453133b03f9b57e73779cfa400b16c2
bl41r <dbsmith.dbs83@gmail.com>
2017-01-26 20:24:53 -0800

    allow renderer in decorator

--- src/main_funcs.py
+++ src/main_funcs.py
@@ -11,7 +11,11 @@ def create_directories(directory_names):
             os.makedirs(directory)
 
 
-def create_route(route_name):
+def create_route(route_name, renderer=None):
     """Return text for a route."""
+
+    template = renderer
+    if renderer:
+        renderer = ", renderer='" + template + "'"
     return '@view_config(route_name="' + route_name + '")\n' + 'def ' +\
         route_name + '(request):\n    ' + 'return {}\n\n'
